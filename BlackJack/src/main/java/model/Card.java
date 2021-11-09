60b593f6e9a4463e0a4757dacc1e24e5e87bbf43
keita_nakazawa <keita.nakazawa@excite-software.net>
2021-11-09 17:48:16 +0900

    2021.11.09ここまで

--- BlackJack/src/main/java/model/Card.java
+++ BlackJack/src/main/java/model/Card.java
@@ -36,7 +36,7 @@ enum Mark {
 	
 	protected String mark;
 	
-	Mark(String mark) {
+	private Mark(String mark) {
 		this.mark = mark;
 	}
 }
@@ -61,12 +61,12 @@ enum Number {
 	protected int point;
 	protected int pointAce;
 	
-	Number(String number, int point) {
+	private Number(String number, int point) {
 		this.number = number;
 		this.point = point;
 	}
 	
-	Number(String number, int point, int pointAce) {
+	private Number(String number, int point, int pointAce) {
 		this.number = number;
 		this.point = point;
 		this.pointAce = pointAce;
